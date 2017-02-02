657816277cc9541c522962d4f244619abec3ae20
iem <iem@iutabgdin202-22.local>
2017-02-02 09:57:19 +0100

    create CheckListItem Class step 6

---
+++ Checklists/CheckListItem.swift
@@ -0,0 +1,24 @@
+//
+//  CheckListItem.swift
+//  Checklists
+//
+//  Created by iem on 02/02/2017.
+//  Copyright © 2017 iem. All rights reserved.
+//
+
+import Foundation
+
+class CheckListItem{
+    
+    var text: String
+    var checked: Bool = false
+    
+    init(aText: String, isChecked: Bool){
+        text = aText
+        checked = isChecked
+    }
+    
+    init(aText: String){
+        text = aText
+    }
+}
