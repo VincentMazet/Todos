4d0063206a312ee3a04efee861ac9a78f5434e43
iem <iem@iutabgdin202-22.local>
2017-02-02 11:51:54 +0100

    new view with navigation for add item steps 14-15-16-17-18

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by iem on 02/02/2017.
+//  Copyright © 2017 iem. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    @IBAction func cancel(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func done(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- Checklists/CheckListViewController.swift
+++ Checklists/CheckListViewController.swift
@@ -31,12 +31,13 @@ class CheckListViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    /* Useless now
     @IBAction func addDummyTodo(_ sender: AnyObject) {
         let dummy : CheckListItem = CheckListItem(aText: "dummy item")
         checkListItems.append(dummy)
         self.tableView.insertRows(at: [IndexPath(row: checkListItems.count - 1, section: 0)
 ], with: UITableViewRowAnimation.automatic)
-    }
+    }*/
 }
 
 extension CheckListViewController{
